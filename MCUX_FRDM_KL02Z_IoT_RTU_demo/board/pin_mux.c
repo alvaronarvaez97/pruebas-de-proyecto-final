b40ae935fdd336c95a430db62e2699a8f52c82c2
LAPTOP-1KAV57JR\hp <alvaronarvaez97@gmail.com>
2021-02-25 18:12:07 -0500

    configuracion de pines y adicion de las librerias en el main principal

--- MCUX_FRDM_KL02Z_IoT_RTU_demo/board/pin_mux.c
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/board/pin_mux.c
@@ -13,6 +13,9 @@ package_id: MKL02Z32VFM4
 mcu_data: ksdk2_0
 processor_version: 8.0.1
 board: FRDM-KL02Z
+pin_labels:
+- {pin_num: '19', pin_signal: ADC0_SE3/PTA8/I2C1_SCL, label: 'J10[6]/ADC0_SE3/I2C1_SCL', identifier: a;EXT_SCL}
+- {pin_num: '20', pin_signal: ADC0_SE2/PTA9/I2C1_SDA, label: 'J10[5]/ADC0_SE2/I2C1_SDA', identifier: EXT_SDA}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
  */
 /* clang-format on */
@@ -139,6 +142,8 @@ I2C_InitPins:
 - pin_list:
   - {pin_num: '23', peripheral: I2C0, signal: SCL, pin_signal: PTB3/IRQ_10/I2C0_SCL/UART0_TX}
   - {pin_num: '24', peripheral: I2C0, signal: SDA, pin_signal: PTB4/IRQ_11/I2C0_SDA/UART0_RX}
+  - {pin_num: '19', peripheral: I2C1, signal: SCL, pin_signal: ADC0_SE3/PTA8/I2C1_SCL, identifier: EXT_SCL}
+  - {pin_num: '20', peripheral: I2C1, signal: SDA, pin_signal: ADC0_SE2/PTA9/I2C1_SDA}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
  */
 /* clang-format on */
@@ -151,8 +156,16 @@ I2C_InitPins:
  * END ****************************************************************************************************************/
 void I2C_InitPins(void)
 {
+    /* Port A Clock Gate Control: Clock enabled */
+    CLOCK_EnableClock(kCLOCK_PortA);
     /* Port B Clock Gate Control: Clock enabled */
     CLOCK_EnableClock(kCLOCK_PortB);
+
+    /* PORTA8 (pin 19) is configured as I2C1_SCL */
+    PORT_SetPinMux(I2C_INITPINS_EXT_SCL_PORT, I2C_INITPINS_EXT_SCL_PIN, kPORT_MuxAlt2);
+
+    /* PORTA9 (pin 20) is configured as I2C1_SDA */
+    PORT_SetPinMux(I2C_INITPINS_EXT_SDA_PORT, I2C_INITPINS_EXT_SDA_PIN, kPORT_MuxAlt2);
 
     /* PORTB3 (pin 23) is configured as I2C0_SCL */
     PORT_SetPinMux(I2C_INITPINS_ACCEL_SCL_PORT, I2C_INITPINS_ACCEL_SCL_PIN, kPORT_MuxAlt2);
--- MCUX_FRDM_KL02Z_IoT_RTU_demo/source/MCUX_FRDM_KL02Z_IoT_RTU_demo.c
+++ MCUX_FRDM_KL02Z_IoT_RTU_demo/source/MCUX_FRDM_KL02Z_IoT_RTU_demo.c
@@ -29,13 +29,17 @@
 #include "sdk_hal_uart0.h"
 #include "sdk_hal_gpio.h"
 #include "sdk_hal_i2c0.h"
+#include "sdk_hal_i2c1.h"
 
 #include "sdk_mdlw_leds.h"
 #include "sdk_pph_mma8451Q.h"
 #include "sdk_pph_ec25au.h"
+#include "sdk_pph_sht3x.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+#define HABILITAR_MODEM_EC25		1
+#define HABILITAR_SENSOR_SHT3X		1
 
 /*******************************************************************************
  * Private Prototypes
@@ -48,9 +52,6 @@
 /*******************************************************************************
  * Local vars
  ******************************************************************************/
-
-uint8_t mensaje_de_texto[]="Hola desde EC25";
-
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
@@ -66,7 +67,14 @@ void waytTime(void) {
  * @brief   Application entry point.
  */
 int main(void) {
-	uint8_t estado_actual_ec25;
+
+	uint8_t ec25_mensaje_de_texto[]="Hola desde EC25";
+	uint8_t ec25_estado_actual ;
+	uint8_t ec25_detectado=0;
+
+	uint8_t sht3x_base_de_tiempo=0;
+	sht3x_data_t sht3x_datos;
+	uint8_t sht3x_detectado=0;
 
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
@@ -74,34 +82,86 @@ int main(void) {
 #ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
     BOARD_InitDebugConsole();
 #endif
-    //inicializa puerto UART0 y solo avanza si es exitoso el proceso
-    if(uart0Inicializar(115200)!=kStatus_Success){	//115200bps
-    	return 0 ;
-    }
 
-    //inicializa puerto I2C0 y solo avanza si es exitoso el proceso
-    if(i2c0MasterInit(100000)!=kStatus_Success){	//100kbps
+    printf("Inicializa UART0:");            //inicializa puerto UART0 y solo avanza si es exitoso el proceso
+    if(uart0Inicializar(115200)!=kStatus_Success){	//115200bps
+    	printf("Error");
     	return 0 ;
     }
+    printf("OK\r\n");
+
+    printf("Inicializa I2C0:");            //inicializa puerto I2C0 y solo avanza si es exitoso el proceso
+    if(i2c0MasterInit(100000)!=kStatus_Success){	//100kbps
+    	printf("Error");
+    	return 0 ;
+    }
+    printf("OK\r\n");
+
+    printf("Inicializa I2C1:");
+    //inicializa puerto I2C1 y solo avanza si es exitoso el proceso
+    if(i2c1MasterInit(100000)!=kStatus_Success){	//100kbps
+    	printf("Error");
+    	return 0 ;
+    }
+    printf("OK\r\n");
+
+#if HABILITAR_SENSOR_SHT3X
+    printf("Detectando SHT3X:");
+    //LLamado a funcion que identifica sensor SHT3X
+    if(sht3xInit()== kStatus_Success){
+    	sht3x_detectado=1;
+    	printf("OK\r\n");
+    }
+#endif
+
 
     //LLamado a funcion que indeitifica acelerometro MMA8451Q
     if (mma8451QWhoAmI() == kStatus_Success){
     	(void)mma8451QInit();	//inicializa acelerometro MMA8451Q
     }
 
-    //inicializa todas las funciones necesarias para trabajar con el modem EC25
-    ec25Inicializacion();
-    ec25EnviarMensajeDeTexto(&mensaje_de_texto[0], sizeof(mensaje_de_texto));
 
-	//Ciclo infinito encendiendo y apagando led verde
-	//inicia el SUPERLOOP
+#if HABILITAR_MODEM_EC25
+    //Inicializa todas las funciones necesarias para trabajar con el modem EC25
+    printf("Inicializa modem EC25\r\n");
+    ec25Inicializacion();
+
+    //Configura FSM de modem para enviar mensaje de texto
+    printf("Enviando mensaje de texto por modem EC25\r\n");
+    ec25EnviarMensajeDeTexto(&ec25_mensaje_de_texto[0], sizeof(ec25_mensaje_de_texto));
+#endif
+
     while(1) {
     	waytTime();		//base de tiempo fija aproximadamente 200ms
 
-		estado_actual_ec25 = ec25Polling();	//actualiza maquina de estados encargada de avanzar en el proceso interno del MODEM
+
+#if HABILITAR_SENSOR_SHT3X
+    	if(sht3x_detectado==1){
+    		sht3x_base_de_tiempo++; //incrementa base de tiempo para tomar dato sensor SHT3X
+			if(sht3x_base_de_tiempo>10){//	>10 equivale aproximadamente a 2s
+				sht3x_base_de_tiempo=0; //reinicia contador de tiempo
+	    		if (sht3xReadData(&sht3x_datos) == kStatus_Success) {//toma lectura humedad, temperatura
+	    			printf("SHT3X ->");
+	    			//printf("temperatura:0x%X ",sht3x_datos.temperatura);	//imprime temperatura sin procesar
+	    			//printf("CRC8_t:0x%X ",sht3x_datos.crc_temperatura);	//imprime CRC8 de temperatura
+        			//printf("humedad:0x%X ",sht3x_datos.humedad);	//imprime humedad sin procesar
+        			//printf("CRC8_h:0x%X ",sht3x_datos.crc_humedad);	//imprime CRC8 de temperatura
+           			printf("temperatura:       %.2f     ",(float)(-45.0+175.0*(sht3x_datos.temperatura/65535.0)));    //imprime temperatura
+        			//printf("CRC8_t:         0x%X               ",sht3x_datos.crc_temperatura);    //imprime CRC8 de temperatura
+        			printf("humedad:         %.2f  %%   ",(float)(100.0*(sht3x_datos.humedad/65535.0)));    //imprime humedad
+        			//printf("CRC8_h:         0x%X             ",sht3x_datos.crc_humedad);    //imprime CRC8 de temperatura
+
+        			printf("\r\n");	//Imprime cambio de linea
+	    		}
+			}
+    	}
+#endif
+
+#if HABILITAR_MODEM_EC25
+    	ec25_estado_actual = ec25Polling();	//actualiza maquina de estados encargada de avanzar en el proceso interno del MODEM
 											//retorna el estado actual de la FSM
 
-    	switch(estado_actual_ec25){
+		switch(ec25_estado_actual){
     	case kFSM_RESULTADO_ERROR:
     		toggleLedRojo();
     		apagarLedVerde();
@@ -127,6 +187,6 @@ int main(void) {
     		break;
     	}
     }
-
+#endif
     return 0 ;
 }
